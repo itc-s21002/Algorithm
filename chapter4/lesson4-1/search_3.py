87e70723da4fa92c0a50ebe990b98fee981f53f7
s21002 <s21002@std.it-college.ac.jp>
2023-06-07 10:04:14 +0900

    end

--- chapter4/lesson4-1/search_3.py
+++ chapter4/lesson4-1/search_3.py
@@ -2,14 +2,21 @@ data = [
     ["佐藤", "000-0000-0000"],
     ["鈴木", "111-1111-1111"],
     ["高橋", "222-2222-2222"],
-    ["田中", "333-3333-3333"]
+    ["田中", "333-3333-3333"],
+    [None]
 ]
-n = len(data)
-s = input("検索する相手の苗字は？ ")
-i = 0
-while i < n and data[i][0] != s:
-    i += 1
-if i == n:
-    print("その名は登録されていません")
-else:
-    print(data[i][0]+"さんの番号は "+data[i][1]+"です")
+while True:
+    s = input("検索する相手の苗字は？ ")
+    i = 0
+    while data[i][0]!= None and data[i][0] != s:
+        i += 1
+    if data[i][0] == None:
+        print("その名は登録されていません")
+        a = input("電話番号を入れてください")
+        data.pop(-1)
+        data.append([s, a])
+        data.append([None])
+
+
+    else:
+        print(data[i][0]+"さんの番号は "+data[i][1]+"です")
